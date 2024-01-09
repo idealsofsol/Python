d7eedc9cb02c4b21f11e503fd80b249f950828ff
IdealSoftSolutions <UserAdmin@123>
2024-01-08 18:19:45 -0600

    if and if else

---
+++ src/main/com/controlstmnts/controlstmnts.py
@@ -0,0 +1,20 @@
+print("enter input number")
+a = int(input())
+if(a%2 == 0):
+    print("a is even")
+
+
+
+print("enter student percentage")
+percentage = int(input())
+if(percentage > 90):
+    print("grade is A")
+elif(percentage > 75):
+    print("grade is B")
+elif(percentage > 65):
+    print("grade is C")
+else:
+    print("fail")
+
+
+
