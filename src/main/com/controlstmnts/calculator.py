94eb1bd89a93814158b0b6ca37e2c8baf0fce487
IdealSoftSolutions <UserAdmin@123>
2024-01-11 13:36:34 -0600

    adding calculator

--- src/main/com/controlstmnts/calculator.py
+++ src/main/com/controlstmnts/calculator.py
@@ -1,16 +1,27 @@
 print("enter value of input1")
-input1=int(input())
+input1 = int(input())
 print("enter value of input2")
-input2=int(input())
-print("select the operation:: sum-->1\n Sub-->2\n Mul-->3\n Div-->4\n")
-operation=int(input())
-if(operation==1):
-    print(f'sum of {input1} and {input2} is :: {input1+input2}')
-elif(operation == 2):
-    print(f'sub of {input1} and {input2} is :: {input1-input2}')
-elif(operation == 3):
-    print(f'mul of {input1} and {input2} is :: {input1*input2}')
-elif(operation == 4):
-    print(f'div of {input1} and {input2} is :: {input1%input2}')
+input2 = int(input())
+print(f'select the operation {input2} :: sum-->1\n Sub-->2\n Mul-->3\n Div-->4\n')
+operation = int(input())
+
+
+def sub():
+    print(f'sub of {input1} and {input2} is :: {input1 - input2}')
+
+
+if (operation == 1):
+    sum()
+elif (operation == 2):
+    sub()
+elif (operation == 3):
+    print(f'mul of {input1} and {input2} is :: {input1 * input2}')
+elif (operation == 4):
+    print(f'div of {input1} and {input2} is :: {input1 % input2}')
 else:
     print('not exsisted')
+
+
+def sum():
+    print(f'sum of {input1} and {input2} is :: {input1 + input2}')
+
