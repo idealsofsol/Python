03cd493b0d894003c7d22c09e1e680d422ca17ab
IdealSoftSolutions <solutionidealsoft@gmail.com>
2024-01-17 14:19:35 -0600

    airthmetic operatins practice

---
+++ src/main/com/practice/airthmeticOperations.py
@@ -0,0 +1,16 @@
+
+
+message = input('enter your input message :: ')
+print(f'your message is :: {message} ')
+
+a = (input('Enter first value  :: '))
+b = (input('Enter second value  :: '))
+
+print(type(a))
+print(f'sum of {a} and {b} is :: {a+b}')
+print(f'difference of {a} and {b} is :: {a-b}')
+print(f'product of {a} and {b} is ::{a*b}')
+
+sasank = input( 'sasank :: ')
+Ramana = input ( 'Ramana :: ')
+print(f'message of {sasank} {Ramana} :: {sasank and Ramana}')
