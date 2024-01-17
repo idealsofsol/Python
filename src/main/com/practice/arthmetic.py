06466593348fcedefd06ae91482b586f507f1c85
IdealSoftSolutions <UserAdmin@123>
2024-01-17 14:11:23 -0600

    arthemetics

---
+++ src/main/com/practice/arthmetic.py
@@ -0,0 +1,17 @@
+
+a = int(input("Enter number of a "))
+b = int(input("Enter number of b "))
+sum = a + b
+difference = a - b
+product = a * b
+division = a // b
+floatDivision = a / b
+print(f'sum of {a} and {b} :: {sum} ')
+print(f'difference of {a} and {b} :: {difference}')
+print(f'product of {a} and {b} :: {product}')
+print(f'division of {a} and {b} :: {division}')
+print(f'floatDivision of {a} and {b} :: {floatDivision}')
+
+
+input=input("Enter your message ")
+print(f'your input message is :: {input}')
