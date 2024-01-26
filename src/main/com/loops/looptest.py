3569dd9d0c3be816fc20a746b8bd9a574da4f74f
IdealSoftSolutions <UserAdmin@123>
2024-01-26 12:26:39 -0600

    practice

--- src/main/com/loops/looptest.py
+++ src/main/com/loops/looptest.py
@@ -58,9 +58,11 @@ class LoopsTest:
         for i in range(size, -1, -1):
             print(list[i])
 
-    def reverse_string(self):
-        m="Anjali"[::-1]
-        print(m)
+    def reverse_string(self, name):
+       # name = "Anjali"
+        size = len(name)
+        for i in range(size-1, -1, -1):
+            print(name[i], end='')
 
 
 #Write a program to use for loop to print the following reverse number pattern
@@ -71,3 +73,81 @@ class LoopsTest:
             for j in range(column_count-i, 0, -1):
                 print(f'{j}',end=' ')
             print(' ')
+
+
+#Display numbers from -10 to -1 using for loop
+    def reverse_numbers(self):
+        i=0
+        for i in range(-10,0):
+            print(i)
+
+
+#Use else block to display a message “Done” after successful execution of for loop
+    def use_else_block(self):
+        for i in range(5):
+            print(i)
+        else:
+            print("Done!")
+
+#Write a program to display all prime numbers within a range
+    def prime_numbers(self, n):
+        for num in range(0,n+1):
+            if num > 0:
+                for i in range(2,num):
+                    if num%i==0:
+                        print('not prime')
+                        break
+
+                else:
+                    print(num)
+
+#Display Fibonacci series up to 10 terms
+    def fibonacci_number(self,number1,number2):
+        print("enter the fibonacci number")
+        for i in range(10):
+            print(number1,end=" ")
+            res = number1 + number2
+
+            number1 = number2
+            number2 = res
+
+#Find the factorial of a given number
+    def factorial_numbers(self,n):
+        factorial = 1
+        for i in range(n,0,-1):
+            factorial = factorial * i
+            print(f'factorial is {i}: ',factorial)
+
+#Reverse a given integer number
+    def reverse_integer_number(self,n):
+        reverse_number = 0
+        while n > 0:
+            remainder = n % 10
+            reverse_number = (reverse_number * 10) + remainder
+            n = n // 10
+            print("reverse number ",reverse_number)
+
+# Use a loop to display elements from a given list present at odd index positions
+    def using_loop_to_display_elements(self,my_list):
+        for i in my_list[1:2]:
+            print(i, end=" ")
+
+#Calculate the cube of all numbers from 1 to a given number
+    def cube(self,n):
+        for i in range(1,n):
+            cube = i * i * i
+            print(f'cube of {i} : {cube}')
+
+
+#
+    def pattern(self):
+        row_count = 5
+        for i in range(1, row_count + 1, 1):
+            for j in range(1, i + 1, 1):
+                print('*', end=' ')
+            print("")
+        row_count = 5
+        for i in range(row_count,0, -1):
+            for j in range(1, i - 1):
+                print('*', end=' ')
+            print("")
