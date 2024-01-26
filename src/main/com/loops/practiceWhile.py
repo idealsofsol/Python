4d8bde6acd5631ee71407dff2d534da042f86923
IdealSoftSolutions <UserAdmin@123>
2024-01-26 12:34:55 -0600

    while loop

---
+++ src/main/com/loops/practiceWhile.py
@@ -0,0 +1,24 @@
+i=1
+while(i<=10):
+    print("Hello World")
+    i=i+1
+
+i=1
+while(i<=20):
+    if i%3==0:
+        print(i)
+    i=i+1
+
+n=int(input('enter the number: '))
+sum=0
+i=0
+while i<=n:
+    sum=sum+i
+    print('The sum of n: ',sum)
+    i=i+1
+
+    
+i=1
+while(i<=10):
+    print(i)
+    i=i+1
