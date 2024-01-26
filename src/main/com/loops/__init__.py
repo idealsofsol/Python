87f9b3214c9c7a0b3ac77179eac3f1b799311896
IdealSoftSolutions <UserAdmin@123>
2024-01-26 12:31:51 -0600

    practice

---
+++ src/main/com/loops/__init__.py
@@ -0,0 +1,30 @@
+from src.main.com.loops.looptest import LoopsTest
+
+if __name__ == '__main__':
+    loop_test = LoopsTest()
+    #loop_test.print_natural_numbers()
+    #loop_test.print_pattern()
+    #loop_test.sum_of_given_number()
+    #loop_test.print_multiplication()
+    #loop_test.list_numbers()
+    #loop_test.total_number_of_digits()
+    #loop_test.reverse_number_pattern()
+    #loop_test.list_in_reverse()
+    #name = "Anjali"
+    #loop_test.reverse_string(name)
+    #loop_test.reverse_numbers()
+    #loop_test.use_else_block()
+    #n = int(input('Enter numbers '))
+    #loop_test.prime_numbers(n)
+    '''number1 = 0
+    number2 = 1
+    loop_test.fibonacci_number(number1,number2)
+    n=int(input('enter the number '))
+    loop_test.factorial_numbers(n)
+    n=int(input('Enter the number '))
+    loop_test.reverse_integer_number(n)
+    my_list=eval(input('Enter the numbers in list '))
+    loop_test.using_loop_to_display_elements(my_list)
+    n=int(input('Enter the number '))
+    loop_test.cube(n)'''
+    loop_test.pattern()
