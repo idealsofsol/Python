f226a1c6b0d709355ec75969fbcfc8960c4f586f
amrutha947 <amruthachamanchula@gmail.com>
2025-09-10 01:05:39 +0530

    src/main/calculater.py

--- src/main/calculater.py
+++ src/main/calculater.py
@@ -7,7 +7,28 @@ def subtract(x,y):
 def multiply(x,y):
   return x*y
  
-def division(x,y):
-  if y!=0:
+def divide4(x,y):
+  if y==0:
     return "Error! division by zero"
   return x/y
+
+print("Select operation:")
+print("1. Addition")
+print("2. Subtraction")
+print("3. Multiplication")
+print("4. Divide")
+choice = input("Enter choice (1/2/3/4): ")
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+if choice == '1':
+    print("Result:", add(num1, num2))
+elif choice == '2':
+    print("Result:", subtract(num1, num2))
+elif choice == '3':
+    print("Result:", multiply(num1, num2))
+elif choice == '4':
+    print("Result:", divide(num1, num2))
+else:
+    print("Invalid Input")
