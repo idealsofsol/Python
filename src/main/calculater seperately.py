b556a58d96e1729bfbac0ecd2f7204d53b82cce6
amrutha947 <amruthachamanchula@gmail.com>
2025-09-10 02:01:45 +0530

    added calculater program seperately

---
+++ src/main/calculater seperately.py
@@ -0,0 +1,22 @@
+def add(x,y):
+ return x+y
+num1=10
+num2=20
+result=add(num1,num2)
+print("The sum of", num1, "and",num2,  "is:",result)
+
+def subtract(x,y):
+ return x-y
+num1=20
+num2=10
+result=subtract(num1,num2)
+
+print("The differance of", num1, "and",num2, "is:",result)
+
+
+def divide(x,y):
+ return x/y
+num1=20
+num2=10
+result=divide(num1,num2)
+print("The divisin of", num1, "and",num2, "is:",result)
