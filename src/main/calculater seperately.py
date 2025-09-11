a669666ac750e83ec6f3ae072645b9d22f211fd7
amrutha947 <amruthachamanchula@gmail.com>
2025-09-11 16:18:15 +0530

    updated separate calculator

--- src/main/calculater seperately.py
+++ src/main/calculater seperately.py
@@ -1,29 +1,34 @@
-def add(x,y):
- return x+y
+def add(x:int,y:int): 
+    return x+y
+
 num1=10
 num2=20
 result=add(num1,num2)
 print("The sum of", num1, "and",num2,  "is:",result)
 
-def subtract(x,y):
- return x-y
+def subtract(x:int,y:int):
+    return x-y
+
 num1=20
 num2=10
 result=subtract(num1,num2)
 
 print("The differance of", num1, "and",num2, "is:",result)
 
-def multiply(x,y):
- return x*y
+def multiply(x:int,y:int):
+    return x*y
+
 num1=20
 num2=10
 result=multiply(num1,num2)
 
 print("The multiplication of", num1, "and",num2, "is:",result)
 
-def divide(x,y):
- return x/y
+def divide(x:int,y:int):
+    return x/y
+
 num1=20
 num2=10
 result=divide(num1,num2)
+
 print("The divisin of", num1, "and",num2, "is:",result)
