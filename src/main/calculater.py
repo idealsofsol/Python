f0d840bb6cc75cab8b582ad38d79d2b81253d7f8
amrutha947 <amruthachamanchula@gmail.com>
2025-09-19 13:10:07 +0530

    modified calculator program used class and object concept

--- src/main/calculater.py
+++ src/main/calculater.py
@@ -4,35 +4,18 @@ class Calculator:
         return x+y
 
     def subtract(self,x:int,y:int):
-        return x-y
+        result = x-y
+        print(f"Subtract of {x} and {y} is :", result)
 
     def multiply(self,x:int,y:int):
-        return x*y
+       multiply_result=x*y
+       print(f"Multiply of {x} and {y} is :", multiply_result)
 
-    def divide(self,x:int,y:int):
+    def division(self,x:int,y:int):
         if y==0:
-            return "Error! division by zero"
-        return x/y
-
-print("Welcome to calculator")
-num1:int = int(input("Enter first number: "))
-num2:int = int(input("Enter second number: ")) 
-
-print("Select operation:")
-print("1. Addition")
-print("2. Subtraction")
-print("3. Multiplication")
-print("4. Divide")
-choice = input("Enter choice (1/2/3/4): ")
-calsi=new calculator()
-if choice == '1':
-    add_result=add(num1,num2)
-    print("Result:", cal.add_result)
-elif choice == '2':
-    print("Result:", cal.subtract(num1, num2))
-elif choice == '3':
-    print("Result:", cal.multiply(num1, num2))
-elif choice == '4':
-    print("Result:", cal.divide(num1, num2))
-else:
-    print("Invalid Input")
+            print("Error! division by zero")
+        else:    
+            division_result = x/y
+            print(f"Division of {x} and {y} is :", division_result)
+    
+    
--- src/main/main.py
+++ src/main/main.py
@@ -1,38 +1,35 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from calculater import Calculator
 
 
+def print_hi(name:str):
+    print(f'Hi, {name}') 
 
-# Press the green button in the gutter to run the script.
+def run_calculator():
+    print("Welcome to calculator")
+    num1:int = int(input("Enter first number: "))
+    num2:int = int(input("Enter second number: ")) 
+
+    print("Select operation:")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    choice = input("Enter choice (1/2/3/4):")
+
+    calsi =  Calculator()
+
+    if choice == '1':
+        calsi.add(num1,num2)
+    elif choice == '2':
+        calsi.subtract(num1,num2)
+    elif choice == '3':
+        calsi.multiply(num1,num2)
+    elif choice == '4':
+        calsi.division(num1,num2)
+    else:
+        print("Invalid Input")
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-print("Welcome to calculator")
-num1:int = int(input("Enter first number: "))
-num2:int = int(input("Enter second number: ")) 
-
-print("Select operation:")
-print("1. Addition")
-print("2. Subtraction")
-print("3. Multiplication")
-print("4. Divide")
-choice = input("Enter choice (1/2/3/4): ")
-calsi=new calculator()
-if choice == '1':
-    add_result=add(num1,num2)
-    print("Result:", add_result)
-elif choice == '2':
-    print("Result:", subtract(num1, num2))
-elif choice == '3':
-    print("Result:", multiply(num1, num2))
-elif choice == '4':
-    print("Result:", divide(num1, num2))
-else:
-    print("Invalid Input")
+    print_hi('Im from main file')
+    run_calculator()
